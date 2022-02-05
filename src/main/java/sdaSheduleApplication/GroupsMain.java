b41072cf00e72fd0872e6242d167d192b7829b37
AgZid <zidoniene.agne@gmail.com>
2022-02-05 09:48:38 +0200

    User menu added 2022-02-05

--- src/main/java/sdaSheduleApplication/GroupsMain.java
+++ src/main/java/sdaSheduleApplication/GroupsMain.java
@@ -2,6 +2,7 @@ package sdaSheduleApplication;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 /**
  * SDA scheduler application
@@ -10,8 +11,8 @@ import java.util.Map;
  * Create class Student extends Person having field hasPreviousJavaKnowledge (boolean)
  * Create class Group having fields name (eg. JavaVilnius11) , one Trainer and list of Students.
  * Create class DataPopulateService which would have static method with a return type of List<Group> and would initialize and return list of 4 groups.
- *     Each group should have 2-5 students, and one trainer.
- *
+ * Each group should have 2-5 students, and one trainer.
+ * <p>
  * Ensure the fact that a group will only have distinct students (How would you do that?)
  * Ensure the fact that a group will only have a maximum of 5 students; When you try to add a 6th one throw an MaximumNumberOfStudentsReached exception
  * Display all students in a group sorted alphabetically by lastName or display ALL students alphabetically.
@@ -25,58 +26,132 @@ import java.util.Map;
  */
 public class GroupsMain {
 
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         List<Group> groups = DataPopulateService.dataInitialisation();
 
         GroupServices groupServices = new GroupServices(groups);
-        System.out.println("Sort students:");
-        List<Student> studentsSorted =  groupServices.sortStudentsAlphabetically("G4");
-        studentsSorted.forEach(student -> System.out.println(student.getLastName() + " " + student.getFirstName()));
+        GroupsMain groupsMain = new GroupsMain();
 
-        System.out.println();
-        System.out.println("Maximum number of students:");
-        System.out.println(groupServices.searchMaxNumberOfStudents());
+        groupsMain.menuOptions();
+        String userChoose = scanner.nextLine();
 
-        System.out.println();
-        System.out.println("All students younger than 25:");
-        groupServices.searchStudentsYoungerThanX(25).forEach(student ->
-                System.out.println(student.getAge() + " m. " + student.getFirstName() + " " + student.getLastName() ));
+        while (!userChoose.equals("0")) {
 
-        System.out.println();
-        System.out.println("All students grouped by trainer that teaches to them:");
+            switch (userChoose) {
+                case "1":
+                    System.out.println();
+                    System.out.println("Enter group name:");
+                    String selectedGroup = scanner.nextLine();
+                    groupsMain.sortStudentsInGroup(groupServices, selectedGroup);
+                    break;
+                case "2":
+                    System.out.println();
+                    System.out.println("Maximum number of students:");
+                    System.out.println(groupServices.searchMaxNumberOfStudents());
+                    break;
+                case "3": {
+                    System.out.println();
+                    System.out.println("Enter age parameter:");
+                    String enteredValue = scanner.nextLine();
+                    groupsMain.showAllStudentsYoungerThanX(groupServices, enteredValue);
+                    break;
+                }
+                case "4":
+                    System.out.println();
+                    System.out.println("All students grouped by trainer that teaches to them:");
+                    groupsMain.showAllStudensGroupedByTrainers(groupServices);
+                    break;
+                case "5":
+                    System.out.println();
+                    System.out.println("All students with previous java knowledge:");
+                    groupServices.searchStudentsHavingJavaKnowledge()
+                            .forEach(student -> System.out.println(student.getFirstName() + " " + student.getLastName()));
+                    break;
+                case "6":
+                    System.out.println();
+                    System.out.println("All trainers who are NOT experienced:");
+                    groupServices.searchTrainersWithNoExperience()
+                            .forEach(trainer -> System.out.println(trainer.getFirstName() + " " + trainer.getLastName()));
+                    break;
+                case "7":
+                    System.out.println();
+                    System.out.println("Group with the highest number of students with no previous java knowledge:");
+                    System.out.println(groupServices.searchGroupWithHighestNumberOfStudentsWithoutJavaKnowledge());
+                    break;
+                case "8": {
+                    System.out.println();
+                    System.out.println("Remove all the students younger than X age from all groups:");
+                    System.out.println("Before remove:");
+                    groupsMain.showAllGroups(groups);
+                    System.out.println("Enter age parameter:");
+                    String enteredValue = scanner.nextLine();
+                    try {
+                        int selectedAge = Integer.parseInt(enteredValue);
+                        groupServices.removeStudentsYoungerThanX(selectedAge);
+                        System.out.println("After remove:");
+                        groupsMain.showAllGroups(groups);
+
+                    } catch (NumberFormatException ex) {
+                        ex.printStackTrace();
+                    }
+                    break;
+                }
+                case "9": {
+                    groupsMain.showAllGroups(groups);
+                    break;
+                }
+            }
+            groupsMain.menuOptions();
+            userChoose = scanner.nextLine();
+        }
+    }
+
+    private void showAllGroups(List<Group> groups) {
+        groups.forEach(group -> group.getStudents()
+                .forEach(student ->
+                        System.out.println(student.getAge() + " m. " + student.getFirstName() + " " + student.getLastName())));
+    }
+
+    private void showAllStudensGroupedByTrainers(GroupServices groupServices) {
         Map<Trainer, List<Student>> trainers = groupServices.provideTrainersAndStudents();
         trainers.forEach((trainer, students) -> {
             System.out.println("Trainer " + trainer.getFirstName() + " " + trainer.getLastName() + " students:");
             students.forEach(student -> System.out.println("-" + student.getFirstName() + " " + student.getLastName()));
         });
+    }
 
+    private void showAllStudentsYoungerThanX(GroupServices groupServices, String enteredValue) {
+        try {
+            int selectedAge = Integer.parseInt(enteredValue);
+            System.out.println("All students younger than " + selectedAge + ":");
+            groupServices.searchStudentsYoungerThanX(selectedAge).forEach(student ->
+                    System.out.println(student.getAge() + " m. " + student.getFirstName() + " " + student.getLastName()));
+        } catch (NumberFormatException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void sortStudentsInGroup(GroupServices groupServices, String selectedGroup) {
+        System.out.println("Sorted students by lastname in group " + selectedGroup + ":");
+        List<Student> studentsSorted = groupServices.sortStudentsAlphabetically(selectedGroup);
+        studentsSorted.forEach(student -> System.out.println(student.getLastName() + " " + student.getFirstName()));
+    }
+
+    private void menuOptions() {
         System.out.println();
-        System.out.println("All students with previous java knowledge:");
-        groupServices.searchStudentsHavingJavaKnowledge()
-                .forEach(student -> System.out.println(student.getFirstName() + " " + student.getLastName()));
-
-        System.out.println();
-        System.out.println("All trainers who are NOT experienced:");
-        groupServices.searchTrainersWithNoExperience()
-                .forEach(trainer -> System.out.println(trainer.getFirstName() + " " + trainer.getLastName()));
-
-        System.out.println();
-        System.out.println("Group with the highest number of students with no previous java knowledge:");
-        System.out.println(groupServices.searchGroupWithHighestNumberOfStudentsWithoutJavaKnowledge());
-
-        System.out.println();
-        System.out.println("Remove all the students younger than 20 from all groups:");
-        System.out.println("Before remove:");
-        groups.forEach(group -> group.getStudents()
-                .forEach(student ->
-                        System.out.println(student.getAge() + " m. " + student.getFirstName() + " " + student.getLastName())));
-
-        groupServices.removeStudentsYoungerThanX(20);
-        System.out.println("After remove:");
-        groups.forEach(group -> group.getStudents()
-                .forEach(student ->
-                        System.out.println(student.getAge() + " m.  " + student.getFirstName() + " " + student.getLastName())));
-
+        System.out.println("Select option:");
+        System.out.println("1 - Sort students in group X");
+        System.out.println("2 - Find maximum number of students");
+        System.out.println("3 - Find all students younger than X age");
+        System.out.println("4 - Show all students grouped by trainer that teaches to them");
+        System.out.println("5 - Show all students with previous java knowledge");
+        System.out.println("6 - Show all trainers who are NOT experienced");
+        System.out.println("7 - Show group with the highest number of students with no previous java knowledge");
+        System.out.println("8 - Remove all the students younger than X age from all groups");
+        System.out.println("9 - Show all groups");
+        System.out.println("0 - Exit menu");
     }
 }
